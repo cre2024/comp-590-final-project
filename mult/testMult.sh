3563d8db2ec3660b863d08054dc9a29be55633f3
angelinasu57 <angelinasu57@gmail.com>
2024-05-01 15:05:18 -0400

    cleaning up unused files/folders

--- mult/testMult.sh
+++
@@ -1,9 +0,0 @@
-python3 share.py 10 x1.txt x2.txt
-python3 share.py 5 y1.txt y2.txt
-python3 genBeavers.py 
-python3 beaverMask.py x1.txt y1.txt abc_1.txt de1.txt
-python3 beaverMask.py x2.txt y2.txt abc_2.txt de2.txt
-python3 beaverCompute.py x1.txt y1.txt de2.txt abc_1.txt z1.txt 1
-python3 beaverCompute.py x2.txt y2.txt de1.txt abc_2.txt z2.txt 2
-python3 merge.py z1.txt z2.txt 
-
