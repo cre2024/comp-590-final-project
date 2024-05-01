33823b52569953d96cc1fb52d6e09c4e4ffba177
angelinasu57 <angelinasu57@gmail.com>
2024-05-01 11:42:47 -0400

    added in sample data csv file and pandas into corr-coef, took out comments/tests

--- corr-coef.py
+++ corr-coef.py
@@ -1,19 +1,15 @@
 from mpyc import statistics
 from mpyc.runtime import mpc
 import numpy as np
+import pandas as pd
 
 async def main(dataset_x, dataset_y):
-    # convert numpy arrays to python lists
-    dataset_x_list = dataset_x.tolist()
-    dataset_y_list = dataset_y.tolist()
 
     secnum = mpc.SecFxp() # secnum = "secure number", using SecFxp() to create a secure number
-    print('Using secure FXP (x):', secnum) # test that it is using a secure fixed point number
-    dataset_x_secfxp = list(map(secnum, dataset_x_list)) # mapping the secure number to the python list -> makes a list of secure numbers
+    dataset_x_secfxp = list(map(secnum, dataset_x)) # mapping the secure number to the python list -> makes a list of secure numbers
 
     secnum1 = mpc.SecFxp()
-    print('Using secure FXP (y):', secnum1)
-    dataset_y_secfxp = list(map(secnum1, dataset_y_list))
+    dataset_y_secfxp = list(map(secnum1, dataset_y))
 
 
     # compute the correlation coefficient using mpyc's correlation function 
@@ -21,10 +17,18 @@ async def main(dataset_x, dataset_y):
 
     # reveal the result
     correlation_coefficient = await mpc.output(correlation_coefficient)
-    print("Correlation Coefficient:", correlation_coefficient) #  example correlation coefficient should be 1.0 (since it has a direct 2x relationship)
+    print("Correlation Coefficient:", correlation_coefficient) 
 
 # Run the MPC protocol
 if __name__ == '__main__':
-    dataset_x = np.array([1, 2, 3, 4, 5])  # example
-    dataset_y = np.array([2, 4, 6, 8, 10])
-    mpc.run(main(dataset_x, dataset_y))
+
+    df = pd.read_csv('sampledata.csv')
+
+    bin_col = ['Sex', 'Age', 'Patient Type', 'Fever', 'Coughing', 'Loss of Taste', 'Diabetes'] 
+    for col in bin_col:
+        df[col] = pd.to_numeric(df[col]).astype(int)
+
+    var1 = df['Diabetes'].tolist()
+    var2 = df['Fever'].tolist()
+
+    mpc.run(main(var1, var2))
